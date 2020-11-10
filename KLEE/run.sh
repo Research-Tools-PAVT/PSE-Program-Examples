8dfc5be8b37f08a652be13a73c86c4a3b328a9ac
Sumit Lahiri <lahiri.phdworks@gmail.com>
2020-11-10 22:49:31 +0530

    Add files via upload

---
+++ KLEE/run.sh
@@ -0,0 +1,12 @@
+rm -rf klee* *.bc *.dot *.out
+clang++ -I ../../home/klee/klee_src/include -c -emit-llvm -g -O1 -Xclang -disable-O0-optnone monty_hall.cpp
+
+klee --print-exectree monty_hall.bc
+klee --print-exectree --set-codeflow monty_hall.bc
+
+ktest-tool klee-last/test000009.ktest
+
+clang++ -fsanitize=address -fsanitize=leak -I ../../home/klee/klee_src/include/ -L ../home/klee/klee_build/lib/ monty_hall.cpp -lkleeRuntest
+KTEST_FILE=klee-last/test000002.ktest ./a.out
+
+echo $?

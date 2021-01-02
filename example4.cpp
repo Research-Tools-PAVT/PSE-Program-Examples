5c36aa8bb446a2f846be8cf7768a2d1f319e8fcd
lahiri.phdworks <lahiri.phdworks@gmail.com>
2021-01-02 13:42:05 +0530

    Correct Dumps Generated !

--- example4.cpp
+++ example4.cpp
@@ -1,14 +1,42 @@
-#include <assert.h>
 #include <klee/klee.h>
-#include <random>
 #include <stdio.h>
 
-int main (void) {
+int main(void)
+{
 	int a;
-	klee_make_symbolic(&a, sizeof(a), "a_pse_sym");
-	if (a > a - 1 && a < a + 1 && a != 10 && a != a * 2) {
-		a = 10;
-	} else {
-		a = 90;
+	int b;
+	int c = 90;
+	klee_make_symbolic(&a, sizeof(a), "a_sym");
+	klee_make_symbolic(&b, sizeof(b), "b_sym");
+	klee_make_symbolic(&c, sizeof(c), "c_sym");
+
+	if (a > b)
+	{
+		c *= 9;
 	}
+	else
+	{
+		if (c > 0)
+		{
+			printf("%d", b);
+		}
+		else
+		{
+			printf("%d", a);
+		}
+	}
+
+	if (a > c)
+	{
+		b *= 8;
+		c *= 2;
+	}
+	else
+	{
+		a *= 2;
+		b *= 3;
+		c *= 4;
+	}
+
+	return 0;
 }

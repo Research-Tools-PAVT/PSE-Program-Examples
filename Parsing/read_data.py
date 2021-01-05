f18f46c44f99b2f13285d77fdd1d78c28977ba4d
lahiri.phdworks <lahiri.phdworks@gmail.com>
2021-01-05 14:44:46 +0530

    json dumping

--- Parsing/read_data.py
+++ Parsing/read_data.py
@@ -1,40 +1,23 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-from sexpdata import loads, dumps
+import json
 
-data = """
-(And (Eq false
-     (Eq N0:(ReadLSB w32 0 a_pse_sym)
-         (Mul w32 2 N0)))
-(Slt (Add w32 4294967295
-               N0:(ReadLSB w32 0 a_pse_sym))
-      N0)
-(And (Slt N0:(ReadLSB w32 0 a_pse_sym)
-           (Add w32 1 N0))
-      (Eq false (Eq 10 N0))))"""
+results = []
+with open('testing.txt', "r") as fileptr:
+    lines = fileptr.read().replace("\n", " ")
+    for elems in lines.split("}"):
+        if len(elems.strip()) != 0:
+            ConstraintObj = elems.split("{")[1].strip()
+            temp = {}
+            for fields in ConstraintObj.split(","):
+                [key, value] = fields.strip().split(":")
+                if "Query" in key.strip():
+                    temp[key.strip()] = value.strip()[1:len(value.strip()) -
+                                                      1].strip().split("\t\t")
+                else:
+                    temp[key.strip()] = value.strip()
+            results.append(temp)
 
-sexpr_parse_result = loads(data)
-
-for elems in sexpr_parse_result:
-    if isinstance(elems, list):
-        for objs in elems:
-            print(objs)
-    else:
-        print(elems)
-
-with open('../example4_klee_out/conds_dump.txt', "r") as fileptr:
-    for lines in fileptr:
-        stripped_line = lines.strip()
-        if "File" in stripped_line:
-            pass
-        elif "Predicate" in stripped_line:
-            pass
-        elif "Current" in stripped_line:
-            pass
-        elif "True" in stripped_line:
-            pass
-        elif "False" in stripped_line:
-            pass
-        else:
-            pass
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(results, f, ensure_ascii=False, indent=4)
--- example4.cpp
+++ example4.cpp
@@ -1,3 +1,7 @@
+// RUN: %clangxx -I../../../include -g -DMAX_ELEMENTS=4 -fno-exceptions -emit-llvm -c -o %t1.bc %s
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --libc=klee --max-forks=25 --write-no-tests --exit-on-error --optimize --disable-inlining --search=nurs:depth --use-cex-cache %t1.bc
+
 #include <klee/klee.h>
 #include <stdio.h>
 

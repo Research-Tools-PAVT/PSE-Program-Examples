90973fc203dd25af8959b4d33fa2120587501c4e
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-03-25 10:28:34 +0530

    WIP : Reservior Sampling -- Zach

--- example3.cpp
+++ example3.cpp
@@ -2,52 +2,59 @@
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --libc=klee --max-forks=25 --write-no-tests --exit-on-error --optimize --disable-inlining --search=nurs:depth --use-cex-cache %t1.bc
 
+#include <assert.h>
+#include <stdio.h>
+#include <vector>
 #include "PSE.h"
 
-int getmax(int a, int b)
+int vector_to_int(std::vector<int> v)
 {
-    return a > b ? a : b;
+    reverse(v.begin(), v.end());
+    int decimal = 1;
+    int total = 0;
+    for (auto &it : v)
+    {
+        total += it * decimal;
+        decimal *= 10;
+    }
+    return total;
 }
 
-int main(void)
+void reservoir_sample(std::vector<int> &input, std::vector<int> &sample, int n, int k)
 {
-    int a, b, c, d;
-
-    klee_make_symbolic(&a, sizeof(a), "a_sym");
-    make_pse_symbolic<int>(&b, sizeof(b), "b_sym", 0, 20);
-    // b_sym >= 0 (this is missing) && b_sym <= 20 (gets generated)
-    make_pse_symbolic<int>(&c, sizeof(c), "c_prob_sym", 0, 800);
-    make_pse_symbolic<int>(&d, sizeof(d), "d_prob_sym", 0, 650);
-
-    if (a > b)
-    // b < a (Not hitting line 41 after this) => No forking needed.
-    // a > b /\ b_sym >= 0 && b_sym <= 20 /\ c >= 0 && c <= 800 ..
+    for (int i = 0; i < k; i++)
     {
-        a = c * 2;
-        b = d * 2;
-    }
-    // Neg(a > b) /\ b_sym >= 0 && b_sym <= 20 /\ c >= 0 && c <= 800 ..
-    else if (c > d)
-    {
-        c = a * 3;
-        d = b * 3;
-    }
-    else
-    {
-        a = getmax(c, d);
-        b = getmax(c, b);
+        sample[i] = input[i];
     }
 
-    if (a >= 0 && b >= 0)
+    for (int i = k; i < n; i++)
     {
-        c = c + 1;
-        d = getmax(a, b);
-    }
+        int j;
 
-    if (c > 0)
+        make_pse_symbolic(&j, sizeof(j), "j_sym", 0, (int)i);
+
+        if (j < k)
+        {
+            sample[j] = input[i];
+        }
+    }
+}
+
+int main()
+{
+    int n = 10;
+    std::vector<int> arr(n);
+    for (size_t i = 0; i < n; i++)
     {
-        b = 0;
+        arr[i] = i + 1;
     }
+    int k;
+    klee_make_symbolic(&k, sizeof(k), "k_sym");
+    klee_assume(k >= 1 && k <= 10);
 
-    return 0;
+    std::vector<int> sample(k);
+    reservoir_sample(arr, sample, n, k);
+
+    std::sort(sample.begin(), sample.end());
+    return vector_to_int(sample);
 }

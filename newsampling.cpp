fc1e0ba95deef4caa7ccf681b17faadc1414f835
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-05-05 21:07:11 +0530

    WIP : New Sampling Implementation

--- newsampling.cpp
+++ newsampling.cpp
@@ -15,41 +15,63 @@
  * @param numerator 
  * @param denominator 
  */
-void setFractionValue(void *addr, int numerator, int denominator)
+void setFractionValue(void *addr, int numerator, int denominator, int index)
 {
+    int choice_num, choice_denom;
+
+    std::string numerator_str("num_");
+    numerator_str += std::to_string(index);
+    numerator_str += "_sym";
+
+    std::string denom_str("denom_");
+    denom_str += std::to_string(index);
+    denom_str += "_sym";
+
+    klee_make_symbolic(&choice_num, sizeof(choice_num), numerator_str.c_str());
+    klee_make_symbolic(&choice_denom, sizeof(choice_denom), denom_str.c_str());
+
+    choice_num = numerator;
+    choice_denom = denominator;
+    *(double *)addr = (double)choice_num / choice_denom;
+
+    klee_dump_symbolic_details(&choice_num, numerator_str.c_str());
+    klee_dump_symbolic_details(&choice_denom, denom_str.c_str());
+    klee_dump_symbolic_details(addr, "choice_value");
 }
 
 int main()
 {
-    double choice_prob;
-    int d;
+    int d, value_d;
+    double choice;
 
-    // forall variables
-    make_pse_symbolic(&d, sizeof(d), "d_pse_sym", 0, 2);
-    klee_make_symbolic(&choice_prob, sizeof(choice_prob), "choice_prob_sym");
+    make_pse_symbolic(&d, sizeof(d), "d_sym", 0, 6);
+    klee_make_symbolic(&value_d, sizeof(value_d), "value_d");
+    klee_make_symbolic(&choice, sizeof(choice), "choice_sym");
 
-    int choice_num, choice_denom;
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> int_distribution(1, 6);
+    d = int_distribution(generator);
+    klee_dump_kquery_state();
 
-    if (d == 0)
-    {
-        int choice_num, choice_denom;
-        klee_make_symbolic(&choice_num, sizeof(choice_num), "choice_numerator_0");
-        klee_make_symbolic(&choice_denom, sizeof(choice_denom), "choice_denominator_0");
-        choice_num = 1;
-        choice_denom = 50;
-        klee_dump_symbolic_details(&choice_num, "choice_numerator");
-        klee_dump_symbolic_details(&choice_denom, "choice_denominator");
-    }
-    else
-    {
-        int choice_num, choice_denom;
-        klee_make_symbolic(&choice_num, sizeof(choice_num), "choice_numerator_1");
-        klee_make_symbolic(&choice_denom, sizeof(choice_denom), "choice_denominator_1");
-        choice_num = 1;
-        choice_denom = 16;
-        klee_dump_symbolic_details(&choice_num, "choice_numerator");
-        klee_dump_symbolic_details(&choice_denom, "choice_denominator");
-    }
+    value_d = d;
+    // COMMENT : Evaluates to true so can't get symbolic expression.
+    klee_assume(value_d == d);
 
-    return 0;
+    klee_dump_symbolic_details(&d, "d_sym");
+
+    (d == 1) ? setFractionValue(&choice, 1, 6, d) : ((d == 2) ? setFractionValue(&choice, 1, 6, d) : ((d == 3) ? setFractionValue(&choice, 1, 6, d) : ((d == 4) ? setFractionValue(&choice, 1, 6, d) : ((d == 5) ? setFractionValue(&choice, 1, 6, d) : ((d == 6) ? setFractionValue(&choice, 1, 6, d) : setFractionValue(&choice, 1, 6, d))))));
+    klee_dump_kquery_state();
 }
+
+// (d == 1) ?
+//     setFractionValue(&choice, 1, 6) :
+//     ((d == 2) ?
+//         setFractionValue(&choice, 1, 6) :
+//             ((d == 3) ?
+//                 setFractionValue(&choice, 1, 6) :
+//                     ((d == 4) ?
+//                         setFractionValue(&choice, 1, 6) :
+//                         ((d == 5) ?
+//                             setFractionValue(&choice, 1, 6) :
+//                             ((d == 6) ?
+//                                 setFractionValue(&choice, 1, 6) : setFractionValue(&choice, 1, 6))))));
--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 clang++ -I $HOME/klee/include -c -emit-llvm -std=c++17 -g -O0 -Xclang -disable-O0-optnone $1.cpp
-
-klee --search=random-state --search=nurs:md2u --write-kqueries $1.bc --set-ptree-dump 
+klee --libc=klee --exit-on-error --optimize --disable-inlining --search=nurs:depth --use-cex-cache --write-kqueries $1.bc --set-ptree-dump 
 ktest-tool klee-last/test000001.ktest
 
-g++ -std=c++14 -march=native -I $HOME/klee/include/ -L $HOME/klee/lib/ $1.cpp -lkleeRuntest
+g++ -std=c++17 -march=native -I $HOME/klee/include/ -L $HOME/klee/lib/ $1.cpp -lkleeRuntest
 KTEST_FILE=klee-last/test000001.ktest ./a.out
 
 rm -rf $1_klee_out/

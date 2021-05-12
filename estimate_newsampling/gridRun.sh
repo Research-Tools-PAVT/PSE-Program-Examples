99811bc8a3277f3699b43047f6cf47f700a487e6
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-05-12 12:00:24 +0530

    feat : Cases of Assert Violation

--- estimate_newsampling/gridRun.sh
+++ estimate_newsampling/gridRun.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
 timeout=$1
-for n in 8 9 10 11;
+for n in 5 6 7 8;
 do 
-    for prob in 0.435 0.455 0.515 0.565 0.606 0.616 0.666;
+    for prob in 0.10 0.20 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.95;
     do
-        for k in 250 500 1000 2000;
+        for k in 8 16 32 64 128 256;
         do
             echo  "[Z3] Running for prob : ${prob}, n-flips : ${n}, k (sum-terms) : ${k} [Saving to models/prob_${prob}_${n}_dir/z3models_${k}_${prob}_${n}.json]"
             mkdir -p models/prob_${prob}_${n}_dir
-            python3 candidates.py $k $n $prob $timeout models/prob_${prob}_${n}_dir/z3models_${k}_${prob}_${n}.json
+            timeout ${timeout}m python3 candidates.py $k $n $prob $timeout models/prob_${prob}_${n}_dir/z3models_${k}_${prob}_${n}.json
         done
     done
 done

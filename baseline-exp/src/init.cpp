ac17e6caba997d236f8ee71f350d00527c786ff3
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-06-23 16:53:56 +0530

    WIP : Baseline Experiments

---
+++ baseline-exp/src/init.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <init.h>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+int main() {
+  /**
+   * @brief Initialize the FORALL Variables.
+   *
+   */
+
+  return 0;
+}

0f1545291e15b0ba2c671851ecd5dc9c998e795c
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-09-13 20:24:37 +0530

    [WIP] BMC Technique for PSE

--- corral/code.c
+++ corral/code.c
@@ -1,34 +1,30 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-float __start(int n, int y) {
+float getProbValueOnePath(int n, int y, float prob, int *sum) {
   int flips_needed = n;
-  /**
-   * Code to use the forall values.
-   * Sample value of the probabilistic variable.
-   * return the the probability mass
-   *
-   * Check for threshold.
-   * Check for distinct probability paths.
-   */
-  return 0.0019f;
+  for (int i = 0; i < flips_needed; i++) {
+  }
+  return 0.019f;
 }
 
 int main(void) {
 
   // BMC will produce only one path unlike
   // multiple paths that KLEE (SymbEx produces)
-  long long int sum = 0LL;
-  int n = 10, y = 90;
-  float prob_weight = 0.80f, sum_prob = 0.00f, prob;
+  int n = __VERIFIER_nondet_uint() % 50, y = __VERIFIER_nondet_uint(), sum = 0;
+  float prob_weight = 0.80f, sum_prob = 0.00f, prob = __VERIFIER_nondet_float();
+
+  __ESBMC_assume(prob > 0.00000f);
+  __ESBMC_assume(prob <= 1.0f);
 
   while (sum_prob <= prob_weight * prob) {
-    sum_prob += __start(n, y);
+    sum_prob += getProbValueOnePath(n, y, prob, &sum);
     printf("Sum Prob : %lf\n", sum_prob);
   }
 
   // BMC tries to fail this.
-  assert(n * prob <= 10 * sum_prob);
-
+  __ESBMC_assert(n * prob <= 100 * sum_prob, "Failed End Assert");
   return 0;
 }
--- corral/main.c
+++ corral/main.c
@@ -1,15 +1,20 @@
 #include <assert.h>
+#include <stdio.h>
 
 int main(void) {
 
-  int a = 0, b = 1000;
+  int a = __VERIFIER_nondet_uint() % 100000000,
+      b = __VERIFIER_nondet_uint() % 100000000;
 
   while (a <= b) {
+    printf("\nLoop Start : %s : %d, %s, %d", "a", a, "b", b);
     a = a + 1;
     b = b - 1;
+    printf("Loop End : %s : %d, %s, %d", "a", a, "b", b);
+    // assert(a > b);
   }
 
   // This assert must fail
-  // /k:1000 /recursionBound:1000 /timeLimit:50000
-  assert(a > 2 * b);
+  // /k:700 /recursionBound:700 /timeLimit:50000
+  assert(a < b - 1);
 }

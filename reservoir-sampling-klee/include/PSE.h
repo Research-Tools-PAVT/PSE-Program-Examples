6d6b8c49054b5f4439671bf2ab1ffc9a5aab0389
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-09-29 14:55:56 +0530

    Edits

---
+++ reservoir-sampling-klee/include/PSE.h
@@ -0,0 +1,54 @@
+#include <klee/klee.h>
+
+// /**
+//  * @brief Set the Fraction Value object addr to (numerator / denominator)
+//  *
+//  * @param addr (pointer)
+//  * @param numerator
+//  * @param denominator
+//  */
+// void setFractionValue(void *addr, int numerator, int denominator, int index)
+// {
+//   int choice_num, choice_denom;
+//   std::string index_str = std::to_string(index);
+
+//   std::string numerator_str("num_");
+//   numerator_str += index_str;
+//   numerator_str += "_sym";
+
+//   std::string denom_str("denom_");
+//   denom_str += index_str;
+//   denom_str += "_sym";
+
+//   std::string setFractionPlaceholder("setFraction_");
+//   setFractionPlaceholder += index_str;
+
+//   klee_make_symbolic(&choice_num, sizeof(choice_num), numerator_str.c_str());
+//   klee_make_symbolic(&choice_denom, sizeof(choice_denom), denom_str.c_str());
+
+//   choice_num = numerator;
+//   choice_denom = denominator;
+//   *(double *)addr = (double)choice_num / choice_denom;
+
+//   klee_dump_symbolic_details(&choice_num, numerator_str.c_str());
+//   klee_dump_symbolic_details(&choice_denom, denom_str.c_str());
+//   klee_dump_symbolic_details(addr, setFractionPlaceholder.c_str());
+//   klee_dump_kquery_state();
+// }
+
+/**
+ * @brief Creates a probabilistic symbolic variable. Specify start and end of
+ * uniform distribution range. [Any order works]
+ *
+ * @param addr (pointer)
+ * @param bytes (size)
+ * @param name
+ * @param min_elem
+ * @param max_elem
+ */
+void make_pse_symbolic(void *addr, size_t bytes, const char *name,
+                       long int min_elem, long int max_elem) {
+  klee_make_symbolic(addr, bytes, name);
+  klee_assume(*(long int *)addr >= min_elem);
+  klee_assume(*(long int *)addr <= max_elem);
+}

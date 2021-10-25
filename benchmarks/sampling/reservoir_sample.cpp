e8bf2f26ca45de67a4178f1909a92ff24aea975d
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-10-25 21:51:49 +0530

    Matrix Populated

--- benchmarks/sampling/reservoir_sample.cpp
+++ benchmarks/sampling/reservoir_sample.cpp
@@ -13,6 +13,7 @@
 #include <time.h>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
 #ifndef MIN_RANGE
@@ -33,11 +34,14 @@ auto all_sampler = std::bind(random_engine_block1, seed);
 
 int main(int argc, char **argv, char **envp) {
 
+  std::vector<std::pair<int, int>> setCounts;
+
   int forall_samples = 10, FORALLS = 0;
-  int RUNMAIN = 50, n = 10, n_given = 0, k = 5, k_given = 0;
+  int RUNMAIN = 50, n = 10, n_given = 0, k = 5, k_given = 0,
+      pick_random_test = 0, print_md = 0;
   int opt = 0;
 
-  while ((opt = getopt(argc, argv, ":f:r:n:k:")) != -1) {
+  while ((opt = getopt(argc, argv, ":f:r:n:k:p:l:")) != -1) {
     switch (opt) {
     case 'f':
       forall_samples = atoi(optarg);
@@ -51,12 +55,21 @@ int main(int argc, char **argv, char **envp) {
     case 'n':
       n = atoi(optarg);
       n_given = 1;
-      printf("Run for each sample: %d\n", RUNMAIN);
+      printf("Total Size (n) Supplied: %d\n", n);
       break;
     case 'k':
       k = atoi(optarg);
       k_given = 1;
-      printf("Run for each sample: %d\n", RUNMAIN);
+      printf("Reservoir Size (k) Supplied: %d\n", k);
+      break;
+    case 'p':
+      pick_random_test = atoi(optarg);
+      std::cerr << "```\n Random Pick \n```\n";
+      printf("Pick Random: %d\n", pick_random_test);
+      break;
+    case 'l':
+      print_md = atoi(optarg);
+      printf("Print Latex/MKD: %d\n", print_md);
       break;
     case '?':
       printf("Unknown option: %c\n", optopt);
@@ -69,21 +82,17 @@ int main(int argc, char **argv, char **envp) {
   }
 
   std::fstream fs;
-  srand(time(NULL));
   float pt = 0.00000f, pr = 0.00000f;
   std::set<std::vector<int>> probSamples;
 
-  // This is the index of the element we want to check for.
-  // Over all the runs, the expected hit-count for this element
-  // should be (n / k) for "perfect" reservoir sampling, given
-  // we do a large number of runs.
-  fs << "\n\tForall Variable : index_picked \n";
-  int index = rand() % (n - 1);
-  fs << "\t\t" << index << "\n";
-  std::cerr
-      << "| Forall"
-      << " | set1(count==1) | set2(count==0) |\n | :---: | :---: | :---: |\n";
+  print_md ? std::cerr << "| Forall"
+                       << " | set1(count==1) | set2(count==0) |\n | :---: | "
+                          ":---: | :---: |\n"
+           : std::cerr << " Forall"
+                       << " & set1(count==1) & set2(count==0) \\\\ \\hline \n";
+
   while (forall_samples--) {
+    srand(time(NULL));
 
     std::string filename =
         std::string("tests/test_") + std::to_string(forall_samples);
@@ -92,16 +101,23 @@ int main(int argc, char **argv, char **envp) {
     std::cout << "\n*** ForAll Sample : " << forall_samples << " ***\n";
 
     if (!n_given)
-      n = 5 + rand() % 1500;
+      n = 5 + rand() % 32413;
     if (!k_given)
-      k = 5 + rand() % 750;
+      k = 5 + rand() % 32413;
 
+    int index1 = pick_random_test ? rand() % (n - 1) : 2;
+
+    // This is the index of the element we want to check for.
+    // Over all the runs, the expected hit-count for this element
+    // should be (n / k) for "perfect" reservoir sampling, given
+    // we do a large number of runs.
+    fs << "\n\tForall Variable : index_picked1 \n\t\t" << index1 << "\n";
     int count = 0, runs = RUNMAIN;
 
     // Sample must be smaller or equal
     // to input size. Can't be greater.
     while (k >= n) {
-      k = 2 + rand() % 750;
+      k = 2 + rand() % 32413;
     }
 
     pr = (float)(double(k) / n);
@@ -163,27 +179,32 @@ int main(int argc, char **argv, char **envp) {
       for (int i = 0; i < k; i++) {
         // See if we have the element we are
         // looking for in the sample.
-        if (arr[index] == sample[i]) {
-          ret = 1;
+        if (arr[index1] == sample[i]) {
+          // Increment the count since we have seen the element.
+          count++;
           break;
         }
       }
 
-      if (ret == 1) {
-        // Increment the count since we have seen the element.
-        count++;
-      }
-
-      std::cout << "\n\t\tRun : " << runs << ", Count : " << count;
+      // std::cout << "\n\t\tRun : " << runs << ", Count : " << count;
     }
 
     probSamples.insert(j_samples);
 
     pt = (float)((double)count / RUNMAIN);
-    std::cout << "\n=== COUNT : " << count << " ===\n";
-    std::cerr << "| Setting" << std::setw(4) << FORALLS - forall_samples
-              << " | " << std::setw(4) << count << " | " << std::setw(4)
-              << RUNMAIN - count << "|\n";
+    std::cout << "\n\033[1;33m=== COUNT : " << count << " ===\033[0m\n";
+
+    print_md
+        ? std::cerr << "| Setting" << std::setw(4) << FORALLS - forall_samples
+                    << " | " << std::setw(4) << count << " | " << std::setw(4)
+                    << RUNMAIN - count << "|\n"
+        : std::cerr << " Setting " << std::setw(4) << FORALLS - forall_samples
+                    << " & " << std::setw(4) << count << "  (" << pt << ")"
+                    << " & " << std::setw(4) << RUNMAIN - count << "  ("
+                    << std::setw(4) << (float)(1.0000 - pt) << ")"
+                    << " \\\\ \\hline \n";
+
+    setCounts.emplace_back(std::make_pair(count, RUNMAIN - count));
 
     fs << "\n\n=== Mass Collected : " << (float)(((float)pt / pr) * 100)
        << "% ===\nActual Prob : " << pt << ", Expected Prob : " << pr << "\n";
@@ -199,12 +220,12 @@ int main(int argc, char **argv, char **envp) {
     fs.close();
   }
 
-  // std::for_each(probSamples.begin(), probSamples.end(),
-  //               [](const std::vector<int> &e) {
-  //                 std::cout << "j_vec : ";
-  //                 for (const auto elem : e)
-  //                   std::cout << elem << ", ";
-  //                 std::cout << "\n\n";
-  //               });
+  std::cerr << std::endl << "```javascript\n";
+  for (const auto &elem : setCounts) {
+    std::cerr << std::setw(10) << "Setting : " << std::setw(5) << elem.first
+              << ", " << std::setw(5) << elem.second << "\n";
+  }
+  std::cerr << std::endl << "```";
+
   return 0;
 }

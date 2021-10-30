ebadb2b6149c59df3f7604b3a4cbb13c3a4d5558
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-10-30 14:45:04 +0530

    WIP : New Sampling

--- src/flips.cpp
+++ src/flips.cpp
@@ -12,46 +12,44 @@ int main(int argc, char *argv[]) {
   /*
     Forall Variables.
   */
-  int b1 = 10, b2 = 10, SUM = 0;
-  double bias1 = 0.0000;
-  double bias2 = 0.0000;
+  int b1, b2, SUM;
+  double bias1;
+  double bias2;
 
   int coin1[3], coin2[3], sum1, sum2;
 
   klee_make_symbolic(&b1, sizeof(b1), "b1_sym");
   klee_make_symbolic(&b2, sizeof(b2), "b2_sym");
-  klee_assume(b1 > 0 && b1 <= 100000);
-  klee_assume(b2 > 0 && b2 <= 100000);
+  klee_assume(b1 >= 0 && b1 <= 100000);
+  klee_assume(b2 >= 0 && b2 <= 100000);
 
-  klee_make_symbolic(&SUM, sizeof(SUM), "SUM_sym");
   make_pse_symbolic(coin1, sizeof(coin1), "coin1_arr_sym", (int)0, int(1));
   make_pse_symbolic(coin2, sizeof(coin2), "coin2_arr_sym", (int)0, int(1));
 
+  klee_make_symbolic(&SUM, sizeof(SUM), "SUM_sym");
   klee_make_symbolic(&sum1, sizeof(sum1), "sum1_sym");
   klee_make_symbolic(&sum2, sizeof(sum2), "sum2_sym");
 
-  bias1 = (double)(b1) / 100000;
-  bias2 = (double)(b2) / 100000;
-
-  klee_assume(sum1 >= 0 && sum1 <= 3);
-  klee_assume(sum2 >= 0 && sum2 <= 3);
-  klee_assume(sum1 + sum2 >= 0 && sum1 + sum2 <= 6);
-
-  // std::bernoulli_distribution d1(bias1);
-  // std::bernoulli_distribution d2(bias2);
-
   // generate 3 flips for coin-1
   for (std::size_t i = 0; i < FLIPS; ++i) {
-    // Uncomment if KLEE not allowed to pick
-    // coin1[i] = rand() > bias1 ? 1 : 0;
+    // Baised Coin-1.
+    if (rand() % 100000 > b1)
+      coin1[i] = 1;
+    else
+      coin1[i] = 0;
+
     sum1 += coin1[i];
     klee_print_expr("Sum_1 = ", sum1);
   }
 
   // generate 3 flips for coin-2
   for (std::size_t i = 0; i < FLIPS; ++i) {
-    // Uncomment if KLEE not allowed to pick
-    // coin2[i] = rand() > bias2 ? 1 : 0;
+    // Baised Coin-1
+    if (rand() % 100000 > b2)
+      coin2[i] = 1;
+    else
+      coin2[i] = 0;
+
     sum2 += coin2[i];
     klee_print_expr("Sum_2 = ", sum2);
   }
--- src/flips_inst.cpp
+++ src/flips_inst.cpp
@@ -21,23 +21,22 @@ int main(int argc, char *argv[]) {
   klee_make_symbolic(&b1, sizeof(b1), "b1_sym");
   klee_make_symbolic(&b2, sizeof(b2), "b2_sym");
 
-  klee_make_symbolic(&SUM, sizeof(SUM), "SUM_sym");
   make_pse_symbolic(coin1, sizeof(coin1), "coin1_arr_sym", (int)0, int(1));
   make_pse_symbolic(coin2, sizeof(coin2), "coin2_arr_sym", (int)0, int(1));
 
+  klee_make_symbolic(&SUM, sizeof(SUM), "SUM_sym");
   klee_make_symbolic(&sum1, sizeof(sum1), "sum1_sym");
   klee_make_symbolic(&sum2, sizeof(sum2), "sum2_sym");
 
-  /* Instrumented Assume Forall : f2 CLASS */
+  /* Instrumented Assume Forall : f2, B1, B3 Prob CLASS */
+  // COMMENT : STILL WIP
   klee_assume(b1 >= 1 && b1 <= 49999);
   klee_assume(b2 >= 70000 && b2 <= 100000);
+  klee_assume(((sum1 + sum2 == 0) || (sum1 + sum2 == 1) || (sum1 + sum2 == 2) ||
+               (sum1 + sum2 == 5)));
 
-  /* Instrumented Assume Forall : B1, B3 Prob CLASS */
-  klee_assume((sum1 + sum2 == 0) || (sum1 + sum2 == 1) || (sum1 + sum2 == 2) ||
-              (sum1 + sum2 == 5));
-
-  bias1 = (double)(b1) / 100000;
-  bias2 = (double)(b2) / 100000;
+  // bias1 = (double)(b1) / 100000;
+  // bias2 = (double)(b2) / 100000;
 
   // std::bernoulli_distribution d1(bias1);
   // std::bernoulli_distribution d2(bias2);
@@ -45,7 +44,11 @@ int main(int argc, char *argv[]) {
   // generate 3 flips for coin-1
   for (std::size_t i = 0; i < FLIPS; ++i) {
     // Uncomment if KLEE not allowed to pick
-    // coin1[i] = rand() > bias1 ? 1 : 0;
+    if (rand() % 100000 > b1)
+      coin1[i] = 1;
+    else
+      coin1[i] = 0;
+
     sum1 += coin1[i];
     klee_print_expr("Sum_1 = ", sum1);
   }
@@ -53,7 +56,11 @@ int main(int argc, char *argv[]) {
   // generate 3 flips for coin-2
   for (std::size_t i = 0; i < FLIPS; ++i) {
     // Uncomment if KLEE not allowed to pick
-    // coin2[i] = rand() > bias2 ? 1 : 0;
+    if (rand() % 100000 > b2)
+      coin2[i] = 1;
+    else
+      coin2[i] = 0;
+
     sum2 += coin2[i];
     klee_print_expr("Sum_2 = ", sum2);
   }

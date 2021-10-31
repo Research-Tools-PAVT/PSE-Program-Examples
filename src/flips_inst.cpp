2b9e613ac3d79a915e641cd29a49f8e98329f79e
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-10-31 08:45:11 +0530

    feat : klee_assume( ) works !

--- src/flips_inst.cpp
+++ src/flips_inst.cpp
@@ -29,19 +29,9 @@ int main(int argc, char *argv[]) {
   klee_make_symbolic(&sum2, sizeof(sum2), "sum2_sym");
 
   /* COMMENT : KLEE ASSUMES from ANALYSIS */
-  /* f1 * b1 + f1 * b3 + f2 * b1 * f2 * b4 */
-  // klee_assume(((b1 >= 1 && b1 <= 49999) && (b2 >= 75000 && b2 <= 100000) &&
-  //              (sum1 + sum2 < 3)) ||
-  //             ((b1 >= 1 && b1 <= 49999) && (b2 >= 75000 && b2 <= 100000) &&
-  //              (sum1 + sum2 == 6)) ||
-  //             ((b1 >= 50000 && b1 <= 100000) && (b2 >= 75000 && b2 <= 100000)
-  //             &&
-  //              (sum1 + sum2 != 5)) ||
-  //             ((b1 >= 50000 && b1 <= 100000) && (b2 >= 75000 && b2 <= 100000)
-  //             &&
-  //              (sum1 + sum2 <= 2)));
-  klee_assume((b1 + b2 >= 195000) || (b1 + b2 <= 20000));
-  /* COMMENT : KLEE ASSUMES from ANALYSIS : END */
+  klee_assume(((b1 < 10000 && b2 < 10000) && (sum1 + sum2 > 3)) ||
+              ((b1 > 85000 && b2 > 90000) && (sum1 + sum2 < 6)) ||
+              ((b1 > 95000 && b2 < 5000) && (sum1 + sum2 <= 4)));
 
   // generate 3 flips for coin-1
   for (std::size_t i = 0; i < FLIPS; ++i) {
@@ -69,3 +59,17 @@ int main(int argc, char *argv[]) {
   klee_print_expr("SUM_FINAL ", SUM);
   return 0;
 }
+
+/* COMMENT : KLEE ASSUMES from ANALYSIS */
+/* f1 * b1 + f1 * b3 + f2 * b1 * f2 * b4 */
+// klee_assume(((b1 >= 1 && b1 <= 49999) && (b2 >= 75000 && b2 <= 100000) &&
+//              (sum1 + sum2 < 3)) ||
+//             ((b1 >= 1 && b1 <= 49999) && (b2 >= 75000 && b2 <= 100000) &&
+//              (sum1 + sum2 == 6)) ||
+//             ((b1 >= 50000 && b1 <= 100000) && (b2 >= 75000 && b2 <= 100000)
+//             &&
+//              (sum1 + sum2 != 5)) ||
+//             ((b1 >= 50000 && b1 <= 100000) && (b2 >= 75000 && b2 <= 100000)
+//             &&
+//              (sum1 + sum2 <= 2)));
+// klee_assume((b1 + b2 >= 195000) || (b1 + b2 <= 20000));

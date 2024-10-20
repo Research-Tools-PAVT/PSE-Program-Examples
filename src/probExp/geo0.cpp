88710f734d012b56e4ddd84e4def2fc6abc3ff45
lahiri-phdworks <lahiri.phdworks@gmail.com>
2024-10-20 21:49:06 +0530

    Implementation for floats

--- src/probExp/geo0.cpp
+++ src/probExp/geo0.cpp
@@ -1,29 +1,35 @@
 #include "klee/klee.h"
 #include <PSE.h>
+#define PRECISION 100
 
-int geo0(int flip) {
+int geo0(int flip, int prob) {
   int X = 0;
   while (flip == 0) {
     unsigned int d;
     make_pse_symbolic<unsigned int>(&d, sizeof(d), "d_symb", 0, 1);
     if (d) {
       flip = 1;
+      prob *= prob;
     } else {
       X = X + 1;
+      prob *= PRECISION - prob;
       mark_state_winning();
     }
   }
-  return X;
+  return prob * X;
 }
 
 int main(void) {
 
   int flip;
-  klee_make_symbolic(&flip, sizeof(flip), "flip_symb");
+  unsigned int prob;
+  klee_make_symbolic(&flip, sizeof(flip), "flip_forall");
+  klee_make_symbolic(&prob, sizeof(prob), "prob_forall");
+  klee_assume(prob > 1 && prob < PRECISION);
+
   flip = 0;
+  int X = geo0(flip, prob);
 
-  int X = geo0(flip);
-
-  expected_value("X_exp_path", X);
+  expected_value("X_exp_path", X / PRECISION);
   return 0;
 }
